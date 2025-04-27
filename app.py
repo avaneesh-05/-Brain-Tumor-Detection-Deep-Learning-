f678b271db9207f080e0e7e42ab967c53fd77bd8
Avaneesh Pharande <148580307+avaneesh-05@users.noreply.github.com>
2025-04-27 20:45:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import os
-import tensorflow as tf
 import numpy as np
 from tensorflow.keras.preprocessing import image
 from PIL import Image
 import cv2
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 
